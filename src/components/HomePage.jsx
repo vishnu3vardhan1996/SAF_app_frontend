05234cf16fd9d1c8477506cf1aa2aacf3d4202a0
Vishnuvardhan Chellamuthu <vishnu@Vishnuvardhans-MacBook-Pro.local>
2023-05-06 19:13:04 +0530

    react

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const HomePage = () => {
+function HomePage() {
   const history = useHistory();
   useEffect(() => {
     history.push("/login");
