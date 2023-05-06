80284ffdc4bfffc704cedb576fc232bb598d35ee
Vishnuvardhan Chellamuthu <vishnu@Vishnuvardhans-MacBook-Pro.local>
2023-05-06 20:14:07 +0530

    react

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,9 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { Login } from "./Login";
 
 const HomePage = () => {
-
-    const navigate = useNavigate();
-    // useEffect(() => {
-    //     // Redirect to login page after 2 seconds
-    //     const redirectTimer = setTimeout(() => {
-    //         <Navigate to={`${process.env.REACT_APP_LOGIN_ROUTE}`} />;
-    //     }, 2000);
-
-
-
-    //     return () => clearTimeout(redirectTimer);
-    // }, []);
-
-    navigate(`${process.env.REACT_APP_LOGIN_ROUTE}`)
+    return(<Login />)
 };
 
 export { HomePage };
