79a34a7cfd7cb1d2cbcdcd1fa0be3436012ee472
Vishnuvardhan Chellamuthu <vishnu@Vishnuvardhans-MacBook-Pro.local>
2023-05-13 01:29:37 +0530

    react

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,18 +3,18 @@ import{apiURL} from "./App";
 
 function SignUp() {
 
-    const [ firstName, setFirstName ] = useState("");
-    const [ lastName, setLastName ] = useState("");
+    // const [ firstName, setFirstName ] = useState("");
+    // const [ lastName, setLastName ] = useState("");
     const [ userName, setUserName ] = useState("");
     const [ pswd, setPswd ] = useState("");
 
-    function handleFirstName(event) {
-        setFirstName(event.target.value);
-    }
+    // function handleFirstName(event) {
+    //     setFirstName(event.target.value);
+    // }
 
-    function handleLastName(event) {
-        setLastName(event.target.value);
-    }
+    // function handleLastName(event) {
+    //     setLastName(event.target.value);
+    // }
 
     function handleUserName(event) {
         setUserName(event.target.value);
@@ -25,11 +25,11 @@ function SignUp() {
     }
 
     return (<div>
-        <form action={`${apiURL}/signup`} method="POST">
-            <label for="first_name_user" className="first_user_details">First Name: </label>
+        <form action={`${apiURL}/signup_27031996_saf`} method="POST">
+            {/* <label for="first_name_user" className="first_user_details">First Name: </label>
             <input id="first_name_user" name="fiName" value={firstName} onChange={handleFirstName} required /> <br /><br />
             <label for="last_name_user" className="last_user_details">Last Name: </label>
-            <input id="last_name_user" name="laname" value={lastName} onChange={handleLastName} required /> <br /><br />
+            <input id="last_name_user" name="laname" value={lastName} onChange={handleLastName} required /> <br /><br /> */}
             <label for="username_user" className="login_user_details">Username: </label>
             <input id="username_user" name="logindetails" value={userName} onChange={handleUserName} required /> <br /><br />
             <label for="username_password" className="login_user_pass_details">Password: </label>
